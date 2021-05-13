1465a43c7c01ae757961b79303174397790189db
Michael Tan <mm260700@gmail.com>
2021-05-13 15:46:06 +0800

    Finished all functionality
    Page will now only let units be dragged into semesters they are allowed in, as well as check for prerequisites

--- StudyPlan/script.js
+++ StudyPlan/script.js
@@ -1,3 +1,4 @@
+
 //Obtain all draggable elements and the containers which house them
 const units = document.querySelectorAll('.unit')
 const containers = document.querySelectorAll('.container')
@@ -17,10 +18,33 @@ containers.forEach(container => {
         const afterElement = getDragAfterElement(container, e.clientX)
         const draggable = document.querySelector('.dragging')
         if (container.childElementCount < 4) {
-            if (afterElement == null) {
-                container.appendChild(draggable)
-            } else {
-                container.insertBefore(draggable, afterElement)
+            if (container.parentElement.classList[0] == draggable.classList[1] || container.parentElement.classList[0] == draggable.classList[2]) {
+                if (container.parentElement.classList[1] == draggable.classList[3] || container.parentElement.classList[1] == draggable.classList[4]) {
+                    if(draggable.innerHTML in prereqs){
+                        const nameslist = []
+                        let names = document.querySelectorAll('.main')
+                        for(let i = 0; i < names.length; i++){
+                            for(let j = 0; j< names[i].children.length; j++){
+                                nameslist.push(names[i].children.item(j).innerHTML)
+                            }
+                        }
+                        if (nameslist.includes(prereqs[draggable.innerHTML])){
+                            if (afterElement == null) { 
+                                container.appendChild(draggable)
+                                nameslist = []
+                            } else {
+                                container.insertBefore(draggable, afterElement)
+                                nameslist = []
+                            }
+                        }
+                    } else{
+                        if (afterElement == null) { 
+                            container.appendChild(draggable)
+                        } else {
+                            container.insertBefore(draggable, afterElement)
+                        }
+                    }
+                }
             }
         }
     })
