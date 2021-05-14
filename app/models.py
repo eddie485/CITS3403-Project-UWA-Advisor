d8db957398122be9c62fcdda36bf1e2b4a816a0f
sohkhar <sohail.kharrazi@gmail.com>
2021-05-14 14:29:36 +0800

    Working on Authentication system and user login.

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -8,6 +9,12 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 #Adding progress class for later when we want to implement individual user progress in a database and connect it via relationships to the primary key user ID
