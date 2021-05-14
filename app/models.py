01000c19ef4907bc10412077fd2e48dfc2e3a146
sohkhar <sohail.kharrazi@gmail.com>
2021-05-14 14:10:05 +0800

    Initialised database, added debug to .env, fixed up content pages, made User class.

--- app/models.py
+++ app/models.py
@@ -3,5 +3,17 @@ from app import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64), index = True, unique = True)
-    email = db.Column(db.String(128), index = True, unique = True, nullable = False)
-    password_hash = db.Column(db.String(128), nullable = False)
+    email = db.Column(db.String(128), index = True, unique = True) #nullable = False)
+    password_hash = db.Column(db.String(128)) #nullable = True)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+
+#Adding progress class for later when we want to implement individual user progress in a database and connect it via relationships to the primary key user ID
+#class Progress(db.Model):
+#    id = db.Column(db.Integer, primary_key=True)
+#    progresspercent = db.Column(db.Integer) - depends how we're going to display the user progress (% or as string (e.g. 100% or "completed: Undergraduate")
+#    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+#    def __repr__(self):
+#        return 'Progress {}>'.format(self.progresspercent);
