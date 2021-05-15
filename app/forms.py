c5aff55331a54f2cec528dd5ad03b9c960d5b698
eddie485 <ervin25basilio@gmail.com>
2021-05-16 01:23:28 +0800

    added new registtration forms and resored css

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
 from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
 from app.models import User
@@ -14,10 +14,12 @@ class RegistrationForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     name = StringField("Name", validators=[DataRequired()])
     username = StringField("Username", validators=[DataRequired()])
+    major = StringField("Major", validators=[DataRequired()])
+    year = IntegerField("Year", validators=[DataRequired()])
     dateofbirth = DateField("Date of Birth", validators=[DataRequired()])
+    studentid = IntegerField("Student ID", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()]) 
-    password2 = PasswordField(
-        "Repeat Password", validators=[DataRequired(), EqualTo("password")])
+    password2 = PasswordField("Repeat Password", validators=[DataRequired(), EqualTo("password")])
     submit = SubmitField("Register") 
 
     def validate_username(self, username):
