e5f1c1aeda693973c9629e213fb93a5313ef0ea0
Michael Tan <mm260700@gmail.com>
2021-05-16 19:25:25 +0800

    added test

--- tests.py
+++ tests.py
@@ -29,6 +29,11 @@ class UserTesting(unittest.TestCase):
         self.assertFalse(user1.check_password('password'))
         self.assertTrue(user1.check_password('passphrase'))
 
+    def test_duplicate_email(self):
+        user1 = User(username='mike', year = 3, email = 'mike@email.com')
+        user2 = User(username='ed', year = 3, email = 'mike@email.com')
+
+        self.assertEqual(user1.email, user2.email)
 
 if __name__ == '__main__':
     unittest.main()
