feae41f367491826983b917226f5e748346ddfc8
eddie485 <ervin25basilio@gmail.com>
2021-05-16 22:28:53 +0800

    profile page can now be edited by user, also added comments and minor changes to some files

--- app/forms.py
+++ app/forms.py
@@ -8,7 +8,8 @@ class LoginForm(FlaskForm):
     username = StringField("Username", validators = [DataRequired()])
     password =  PasswordField("Password", validators = [DataRequired()])
     remember_me = BooleanField("Remember Me")
-    submit = SubmitField("Sign In")
+    submit = SubmitField("Log In")
+
 
 class RegistrationForm(FlaskForm): 
     email = StringField("Email", validators=[DataRequired(), Email()])
@@ -25,9 +26,32 @@ class RegistrationForm(FlaskForm):
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None: 
-            raise ValidationError("Please select a different username.") 
+            raise ValidationError("That username is already being used. Please select a different username.") 
     
+    # validation to check whether the variable entered by the user already exists in the database.
+    # asks them to try again if it does
+
     def validate_email(self, email): 
         user = User.query.filter_by(email=email.data).first() 
         if user is not None: 
-            raise ValidationError("Please enter a different email address.") 
+            raise ValidationError("That email is already being used. Please enter a different email address.")
+
+    def validate_username(self, studentid):
+        user = User.query.filter_by(studentid=studentid.data).first()
+        if user is not None: 
+            raise ValidationError("That Student ID is already being used. Try again")  
+
+
+""" EditProfileForm is used in the profileEdit.html page. It allows users to change 
+their account details and save them into the database, provided that it does
+not already exist. Works practically the same as the register form"""
+
+class EditProfileForm(FlaskForm): 
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    name = StringField("Name", validators=[DataRequired()])
+    username = StringField("Username", validators=[DataRequired()])
+    major = StringField("Major", validators=[DataRequired()])
+    year = IntegerField("Year", validators=[DataRequired()])
+    dateofbirth = DateField("Date of Birth", validators=[DataRequired()])
+    studentid = IntegerField("Student ID", validators=[DataRequired()])
+    submit = SubmitField("Save Changes") 
--- app/routes.py
+++ app/routes.py
@@ -1,41 +1,57 @@
 from app import app, db
 from app.models import User
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from flask import render_template, request, redirect, url_for, flash
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
+
+# Main/index route is the login page
+# Users are asked to put in their credentials for log in, assuning they already have an account
+# If the input passes validation they are redirected to the homepage
 
 @app.route("/", methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('homePage'))
+
     form = LoginForm()
+
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash("Invalid username or password")
             return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data) 
 
+        login_user(user, remember=form.remember_me.data) 
         next_page = request.args.get('next')
+
         if not next_page or url_parse(next_page).netloc != '': 
             next_page = url_for('homePage') 
         return redirect(next_page)
+
     return render_template('welcome-login.html', title='Welcome', form=form)
 
+# OLD ADMIN LOGIN
+#
 #    error = None 
 #    if request.method == "POST":
 #        if request.form["username"] != "admin" or request.form["pass"] != "adminpass":
 #            error = "You do not have access or your credentials are wrong"
 #        else:
 #         return redirect(url_for("homePage"))
-
+#
 #    return render_template("welcome-login.html", title="Sign in", error=error)
+#
 
 @app.route("/logout") 
 def logout(): 
     logout_user()
     return redirect(url_for('login'))
+
+
+# Route for welcome-registration.html
+# Uses RegistrationForm and asks new users for their details
+# Details are then validated to make sure there are no replicates of the unique ones before they are all saved into the database
 
 @app.route("/registration", methods=["GET", "POST"])
 def registration():
@@ -43,19 +59,56 @@ def registration():
         return redirect(url_for("homePage"))
     form = RegistrationForm()
     if form.validate_on_submit(): 
-        user = User(username=form.username.data, email=form.email.data, name=form.name.data, dateofbirth=form.dateofbirth.data,
-                    major=form.major.data, studentid=form.studentid.data, year=form.year.data)
+        user = User(username=form.username.data, email=form.email.data, name=form.name.data, 
+                                        dateofbirth=form.dateofbirth.data, major=form.major.data, 
+                                                studentid=form.studentid.data, year=form.year.data)
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash("Congratulations! You are not registered with UWA Advisor") 
+        flash("Congratulations! You are now registered with UWA Advisor") 
         return redirect(url_for("login"))
     return render_template("welcome-registration.html",  title="Create an Account", form=form)
 
 @app.route("/profile")
 @login_required
 def profile():
-    return render_template("profile.html", title="Your Profile")
+    form = EditProfileForm()
+    return render_template("profile.html", title="Your Profile", form=form)
+
+
+# Route for profileEdit.html
+# Shows user's current account info and also lets users edit their information, 
+# the form then overwrites it's values in the database
+
+@app.route("/profile-edit", methods=['GET', 'POST'])
+@login_required
+def profileEdit():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        current_user.major = form.major.data
+        current_user.year = form.year.data
+        current_user.dateofbirth = form.dateofbirth.data
+        current_user.studentid = form.studentid.data
+        current_user.name = form.name.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+        form.major.data = current_user.major
+        form.year.data = current_user.year
+        form.dateofbirth.data = current_user.dateofbirth
+        form.studentid.data = current_user.studentid
+        form.name.data = current_user.name
+
+    return render_template('profileEdit.html', title='Edit Profile', form=form)
+
+
+
+# The remaining routes bellow are all links to their respective html pages
 
 @app.route("/content-definitions")
 def contentDefinitions():
