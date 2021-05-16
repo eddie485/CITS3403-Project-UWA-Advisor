93225a2069cdbe7ab1f77877c55bc21659cb065d
sohkhar <sohail.kharrazi@gmail.com>
2021-05-16 13:55:51 +0800

    Minor changes.

--- app/forms.py
+++ app/forms.py
@@ -30,4 +30,4 @@ class RegistrationForm(FlaskForm):
     def validate_email(self, email): 
         user = User.query.filter_by(email=email.data).first() 
         if user is not None: 
-            raise ValidationError("Please enter a different email address.") 
+            raise ValidationError("Please enter a different email address.")
