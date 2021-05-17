b15f149028d649bd9266335692b53c6a9536b341
RoyalJacky <75690080+RoyalJacky@users.noreply.github.com>
2021-05-17 18:11:20 +0800

    etc

--- app/routes.py
+++ app/routes.py
@@ -1,40 +1,63 @@
 from app import app, db
+<<<<<<< HEAD
 from app.models import User, Score
 from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from flask import render_template, request, redirect, url_for, flash, jsonify
+=======
+from app.models import User
+from app.forms import LoginForm, RegistrationForm
+from flask import render_template, request, redirect, url_for, flash
+>>>>>>> parent of a3cf1b2 (added test data into db)
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 
+#The below code is adapted from Miguel Grinberg's Flask Megatutorial to fit our project's functionality.
 @app.route("/", methods=['GET', 'POST'])
 def login():
+    #Following two lines redirect the user to the UWA Advisor home page once login is authenticated.
     if current_user.is_authenticated:
         return redirect(url_for('homePage'))
+
+    #Initialises form for the login
     form = LoginForm()
 
     if form.validate_on_submit():
+        #Queries the database in order to find the user based on username. "filter_by" returns full objects that include a matching username. 
+        #.first() returns the user if they exist, and None if they do not.
         user = User.query.filter_by(username=form.username.data).first()
+
+        #Checks if the user exists OR if their password hash is correct.
         if user is None or not user.check_password(form.password.data):
             flash("Invalid username or password")
             return redirect(url_for('login'))
+        
+        #Logs in user and registers them as "logged in" and also remembers them if they return to the site. 
         login_user(user, remember=form.remember_me.data) 
 
+        #Redirects user to site they were trying to previously access after login. Returns to home page otherwise.
         next_page = request.args.get('next')
 
         if not next_page or url_parse(next_page).netloc != '': 
             next_page = url_for('homePage') 
         return redirect(next_page)
+<<<<<<< HEAD
     return render_template('welcome-login.html', title='Welcome', form=form)
+=======
+    return render_template('welcome-login.html', form=form)
+>>>>>>> parent of a3cf1b2 (added test data into db)
 
 @app.route("/logout") 
 def logout(): 
     logout_user()
     return redirect(url_for('login'))
 
+#The below code is adapted from Miguel Grinberg's Flask Megatutorial to fit our project's functionality.
 @app.route("/registration", methods=["GET", "POST"])
 def registration():
     if current_user.is_authenticated:
         return redirect(url_for("homePage"))
     form = RegistrationForm()
+    #Creates username, email, name, date of birth, major, studentID and year and inserts into database.
     if form.validate_on_submit(): 
         user = User(username=form.username.data, email=form.email.data, name=form.name.data, 
                                         dateofbirth=form.dateofbirth.data, major=form.major.data, 
@@ -42,7 +65,7 @@ def registration():
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash("Congratulations! You are not registered with UWA Advisor") 
+        flash("Congratulations! You are now registered with UWA Advisor") 
         return redirect(url_for("login"))
     return render_template("welcome-registration.html",  title="Create an Account", form=form)
 
@@ -143,30 +166,6 @@ def studyPlanDS():
 @app.route("/study-plan-FINA")
 def studyPlanFINA():
     return render_template("studyPlanFINA.html", title="Study Plan - Finance")
-
-@app.route("/submit/<int:quiz_id>", methods=["PUT"])
-def submit(quiz_id):
-    if request.method == "PUT":
-        data = request.get_json()
-        score = Score.query.filter_by(userid=current_user.id, quiz_id = quiz_id).first()
-        print("recieve score put request: score={} in quiz {}".format(data['score'], quiz_id))
-
-        if score is None:
-            print("score is none")
-            print("score set to {}".format(data['score']))
-            new_score = Score(
-                score=data['score'],
-                quiz_id = quiz_id,
-                userid= current_user.id)
-            db.session.add(new_score)
-            db.session.commit()
-
-        elif score.score < data['score']:
-            score.score = data['score']
-            print("update score to {}".format(score.score))
-            db.session.commit()
-
-    return jsonify({"status": "Ok"})
 
 if __name__=="__main__":
     app.run(debug=True)
