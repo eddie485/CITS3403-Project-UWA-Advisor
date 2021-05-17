511d0c77c5ff71c87fd1abfe5761a16473754ff6
sohkhar <sohail.kharrazi@gmail.com>
2021-05-17 13:43:34 +0800

    Added more comments, fixed .gitignore file.

--- app/routes.py
+++ app/routes.py
@@ -5,50 +5,54 @@ from flask import render_template, request, redirect, url_for, flash
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 
+#The below code is adapted from Miguel Grinberg's Flask Megatutorial to fit our project's functionality.
 @app.route("/", methods=['GET', 'POST'])
 def login():
+    #Following two lines redirect the user to the UWA Advisor home page once login is authenticated.
     if current_user.is_authenticated:
         return redirect(url_for('homePage'))
+
+    #Initialises form for the login
     form = LoginForm()
     if form.validate_on_submit():
+        #Queries the database in order to find the user based on username. "filter_by" returns full objects that include a matching username. 
+        #.first() returns the user if they exist, and None if they do not.
         user = User.query.filter_by(username=form.username.data).first()
+
+        #Checks if the user exists OR if their password hash is correct.
         if user is None or not user.check_password(form.password.data):
             flash("Invalid username or password")
             return redirect(url_for('login'))
+        
+        #Logs in user and registers them as "logged in" and also remembers them if they return to the site. 
         login_user(user, remember=form.remember_me.data) 
 
+        #Redirects user to site they were trying to previously access after login. Returns to home page otherwise.
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '': 
             next_page = url_for('homePage') 
         return redirect(next_page)
-    return render_template('welcome-login.html', title='Welcome', form=form)
-
-#    error = None 
-#    if request.method == "POST":
-#        if request.form["username"] != "admin" or request.form["pass"] != "adminpass":
-#            error = "You do not have access or your credentials are wrong"
-#        else:
-#         return redirect(url_for("homePage"))
-
-#    return render_template("welcome-login.html", title="Sign in", error=error)
+    return render_template('welcome-login.html', form=form)
 
 @app.route("/logout") 
 def logout(): 
     logout_user()
     return redirect(url_for('login'))
 
+#The below code is adapted from Miguel Grinberg's Flask Megatutorial to fit our project's functionality.
 @app.route("/registration", methods=["GET", "POST"])
 def registration():
     if current_user.is_authenticated:
         return redirect(url_for("homePage"))
     form = RegistrationForm()
+    #Creates username, email, name, date of birth, major, studentID and year and inserts into database.
     if form.validate_on_submit(): 
         user = User(username=form.username.data, email=form.email.data, name=form.name.data, dateofbirth=form.dateofbirth.data,
                     major=form.major.data, studentid=form.studentid.data, year=form.year.data)
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash("Congratulations! You are not registered with UWA Advisor") 
+        flash("Congratulations! You are now registered with UWA Advisor") 
         return redirect(url_for("login"))
     return render_template("welcome-registration.html",  title="Create an Account", form=form)
 
